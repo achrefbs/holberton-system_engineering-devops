709405e3cd2ac768bfe928d57b002a9472e86981
achrefbs <siachrefboularess@gmail.com>
2020-08-04 17:38:34 +0100

    task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
 exec { 'cm':
-  command  => 'echo "\tPasswordAuthentication no\n\tIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+  command  => 'echo "    PasswordAuthentication no\n    IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
   provider => 'shell',
 }
